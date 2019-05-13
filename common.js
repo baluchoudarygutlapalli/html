d36c2848fcf274f0edbdf1e8d549ce6ccfc62571
baluchoudarygutlapalli <50576265+baluchoudarygutlapalli@users.noreply.github.com>
2019-05-13 18:30:56 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,9 @@
+function addNos(headVar,bodyVar) 
+
+{ 
+ document.write("the actual script is in external script file called "+"common.js  <hr></br>");
+ var sum = headVar+bodyVar;
+ 
+document.write("the sum of the variables headVar and bodyVar is:"+sum);
+ 
+} 
---
+++ dispHello.js
@@ -0,0 +1,4 @@
+function dispHello()
+{
+document.write('hello world')
+}
